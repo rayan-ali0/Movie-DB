7b84441e08a95c6bc766f1815c45ac992628fa89
Rayan ALi <rayan.ali.8205@gmail.com>
2023-09-22 16:03:58 +0300

    fixing the routers

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 var dateTime = new Date();
 
 app.get('/test', (req, res) => {
-res.json({ status: 200, message: "ok" })
+    res.json({ status: 200, message: "ok" })
 })
 app.get('/time', (req, res) => {
     currentTime = `${dateTime.getHours()}:${dateTime.getMinutes()}`;
@@ -27,37 +27,63 @@ app.get(`/hello/:Id?`, (req, res) => {
 })
 app.get(`/search`, (req, res) => {
     let search = req.query.s;// I put .s in order to take just the variable s in the url if I used req.query it will take all variable in the url and put them in an object
-    search ? res.json({ status: 200, message: "ok", data: search }): 
-    res.status(500).json({ status: 500, error: true, message: "you have to provide a search" })
+    search ? res.json({ status: 200, message: "ok", data: search }) :
+        res.status(500).json({ status: 500, error: true, message: "you have to provide a search" })
 })
-app.get(`/movies/:cmd?/:order?`, (req, res) => {
-    let cmd = req.params.cmd;
-    cmd ? /*  */
-        cmd === "add" ?
-            res.send(`Add a new Movie`) :
-            cmd === "edit" ?
-                res.send(`Update a movie`) :
-                cmd === "delete" ?
-                    res.send(`Delete a movie`) :
-                    cmd === "get" ?
-                    !req.params.order ?   
-                         res.json({ status: 200, data: movies }) :
-                          req.params.order === "by-date" ?
-                             res.json({ status: 200, data: movies.sort((movie1, movie2) => movie1.year - movie2.year) }) :
-                             req.params.order === "by-rating" ?
-                                       res.json({ status: 200, data: movies.sort((movie1, movie2) => movie1.rating - movie2.rating) }):
-                                       req.params.order === "by-title" ?
-                                     res.json({ status: 200, data: movies.sort((movie1, movie2) => movie1.title.localeCompare(movie2.title)) } ):
-                                     res.send("choose another order") :
-                    
-                  res.send(`please insert a valid command`) : 
-                  res.send(`Error`) 
-        
+// app.get(`/movies/:cmd?/:order?`, (req, res) => {
+//     let cmd = req.params.cmd;
+//     cmd ? /*  */
+//         cmd === "add" ?
+//             res.send(`Add a new Movie`) :
+//             cmd === "edit" ?
+//                 res.send(`Update a movie`) :
+//                 cmd === "delete" ?
+//                     res.send(`Delete a movie`) :
+//                     cmd === "get" ?
+//                     !req.params.order ?   
+//                          res.json({ status: 200, data: movies }) :
+//                           req.params.order === "by-date" ?
+//                              res.json({ status: 200, data: movies.sort((movie1, movie2) => movie1.year - movie2.year) }) :
+//                              req.params.order === "by-rating" ?
+//                                        res.json({ status: 200, data: movies.sort((movie1, movie2) => movie1.rating - movie2.rating) }):
+//                                        req.params.order === "by-title" ?
+//                                      res.json({ status: 200, data: movies.sort((movie1, movie2) => movie1.title.localeCompare(movie2.title)) } ):
+//                                      res.send("choose another order") :
 
+//                   res.send(`please insert a valid command`) : 
+//                   res.send(`Error`) 
+
+
+
+// })
+
+app.get(`/movies/add`, (req, res) => {
+    res.send(`Add a new Movie`)
 
 })
 
+app.get(`/movies/edit`, (req, res) => {
+    res.send(`Update a movie`)
+})
 
+app.get(`/movies/delete`, (req, res) => {
+    res.send(`Delete a movie`)
+})
+
+app.get(`/movies/get`, (req, res) => {
+    res.json({ status: 200, data: movies })
+
+})
+app.get(`/movies/get/:order?`, (req, res) => {
+    req.params.order === "by-date" ?
+        res.json({ status: 200, data: movies.sort((movie1, movie2) => movie1.year - movie2.year) }) :
+        req.params.order === "by-rating" ?
+            res.json({ status: 200, data: movies.sort((movie1, movie2) => movie1.rating - movie2.rating) }) :
+            req.params.order === "by-title" ?
+                res.json({ status: 200, data: movies.sort((movie1, movie2) => movie1.title.localeCompare(movie2.title)) }) :
+                res.send("choose another order")
+ 
+ })
 app.listen(port, () => {
     console.log(`app listening on port ${port}`)
 })

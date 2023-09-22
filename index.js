0ed31cc249f64b3509321a4c0ad53ab6d330aeb6
Rayan ALi <rayan.ali.8205@gmail.com>
2023-09-22 10:43:31 +0300

    step 3

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express=require('express')
 const app=express()
 const port =3000
 
-
 app.get('/',async(req,res)=>{
     res.send("OK!")
 })
+var dateTime=new Date();
+
+app.get('/test',async(req,res)=>{
+    res.send({status:200,message:"ok"})
+})
+app.get('/time',async(req,res)=>{
+    currentTime=`${dateTime.getHours()}:${dateTime.getMinutes()}`;
+    res.send({status:200,message:currentTime})})
 
 app.listen(port,()=>{
     console.log(`app listening on port ${port}`)

90105d0b8b27e82fd5dcc761dfe3cbe44e956ff1
Rayan ALi <rayan.ali.8205@gmail.com>
2023-09-22 12:55:47 +0300

    step 4

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
-const express=require('express')
-const app=express()
-const port =3000
+const express = require('express')
+const app = express()
+const port = 3000
 
-app.get('/',async(req,res)=>{
+
+app.get('/', (req, res) => {
     res.send("OK!")
 })
-var dateTime=new Date();
+var dateTime = new Date();
 
-app.get('/test',async(req,res)=>{
-    res.send({status:200,message:"ok"})
+app.get('/test', (req, res) => {
+    res.send({ status: 200, message: "ok" })
 })
-app.get('/time',async(req,res)=>{
-    currentTime=`${dateTime.getHours()}:${dateTime.getMinutes()}`;
-    res.send({status:200,message:currentTime})})
+app.get('/time', (req, res) => {
+    currentTime = `${dateTime.getHours()}:${dateTime.getMinutes()}`;
+    res.send({ status: 200, message: currentTime })
+})
 
-app.listen(port,()=>{
+app.get(`/hello/:Id?`,(req,res)=>{
+    let id=req.params.Id;
+    id ? res.send(`Hello ${id}`) : res.send("Hello")
+})
+app.get(`/search`,(req,res)=>{
+    let search=req.query.s;// I put .s in order to take just the variable s in the url if I used req.query it will take all variable in the url and put them in an object
+   search ? res.send({status:200,message:"ok",data:search}) : res.send({status:500,error:true,message:"you have to provide a search"})
+})
+
+
+app.listen(port, () => {
     console.log(`app listening on port ${port}`)
 })

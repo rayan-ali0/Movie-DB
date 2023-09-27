81f7ed084ad7dd93bca12f2e2be72dace86f864d
Rayan ALi <rayan.ali.8205@gmail.com>
2023-09-27 21:16:36 +0300

    CRUD operation for users

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ const express = require('express')
 require('dotenv').config();
 const app = express()
 const port = process.env.PORT
-// const movieModel = require("./models/movie") 
-// require the mangoose package
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 const mongoDB = process.env.MONGO_DB;
+const movieController = require('./controllers/movieController');
 
 mongoose.connect(mongoDB)
     .then(() => {
@@ -19,14 +18,14 @@ mongoose.connect(mongoDB)
     }
 
     )
-// const movies = [
-//     { title: 'Jaws', year: 1975, rating: 8 },
-//     { title: 'Avatar', year: 2009, rating: 7.8 },
-//     { title: 'Brazil', year: 1985, rating: 8 },
-//     { title: 'الإرهاب و الكباب ', year: 1992, rating: 6.2 }
-// ]
 
-const movieController = require('./controllers/movieController');
+const users = [
+    { username: 'John', password: '1234' },
+    { username: 'Jane', password: '5678' },
+    { username: 'Dohm', password: '9101' }
+
+];
+
 
 
 app.get('/', (req, res) => {
@@ -53,7 +52,49 @@ app.get(`/search`, (req, res) => {
 })
 
 
-app.get("/movies", (req, res) => res.json(movies))
+app.get("/users/get", (req, res) => {
+    let userName = req.query.name;
+    if (userName) {
+        let user = users.find((user) => user.username === userName)
+        res.json(user)
+    }
+    res.json(users)
+}
+)
+
+app.get("/users/get/order", (req, res) => {
+    res.json({ status: 200, data: users.sort((user1, user2) => user1.username.localeCompare(user2.username)) })
+}
+)
+app.post("/users/add",(req,res)=>{
+    let {username,password}=req.query;
+    const newUser = { username:username, password:password}
+     users.push(newUser);
+    res.json(newUser);
+})
+app.delete("/users/delete/:username",(req,res)=>{
+    let userName = req.params.username;
+     const index=users.findIndex((user)=>user.username===userName)
+    if(index===-1){
+        res.json({error:"user not found"});
+    }
+    else{
+        users.splice(index,1)
+        res.json(users);
+    }
+})
+
+function isAuthenticated(req,res,next){
+    const auth=req.headers.authorization
+    const index=users.findIndex((user)=>user.username===auth && user.password===auth)
+if(index!==-1){
+next();
+}
+else{
+    res.status(401).Json({error:"Access Forbidden"})
+}
+}
+
 
 app.get('/movies/get', movieController.get)
 
@@ -61,13 +102,20 @@ app.get(`/movies/get/:order`, movieController.getByOrder)
 
 app.get(`/movies/get/id/:ID`, movieController.getById)
 
-app.post("/movies/add", movieController.add);
+app.post("/movies/add",isAuthenticated, movieController.add);
 
-app.delete('/movies/delete/:ID', movieController.deleteById)
+app.delete('/movies/delete/:ID',isAuthenticated, movieController.deleteById)
 
-app.put("/movies/edit/:ID", movieController.editById)
+app.put("/movies/edit/:ID",isAuthenticated, movieController.editById)
 
 
+
+// const movies = [
+//     { title: 'Jaws', year: 1975, rating: 8 },
+//     { title: 'Avatar', year: 2009, rating: 7.8 },
+//     { title: 'Brazil', year: 1985, rating: 8 },
+//     { title: 'الإرهاب و الكباب ', year: 1992, rating: 6.2 }
+// ]
 
 // app.get(`/movies/:cmd?/:order?`, (req, res) => {
 //     let cmd = req.params.cmd;
@@ -94,7 +142,6 @@ app.put("/movies/edit/:ID", movieController.editById)
 
 // })
 
-//Done--------------------
 
 // app.post(`/movies/add`, async (req, res) => {
 //     let { title, year, rating } = req.query;
